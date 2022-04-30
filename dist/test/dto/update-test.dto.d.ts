7c1d3c202d9566db4ed0f9a2274212c19463e471
Inaku Joel <inakujoel16@gmail.com>
2022-04-30 20:13:36 +0100

    removed test microservice

--- dist/test/dto/update-test.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateTestDto } from './create-test.dto';
-declare const UpdateTestDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateTestDto>>;
-export declare class UpdateTestDto extends UpdateTestDto_base {
-    id: number;
-}
-export {};
